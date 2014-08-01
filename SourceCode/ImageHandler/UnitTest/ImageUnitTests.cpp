abade12fc62a1e1b03af177acd42efcead80ad05
Damian Montenegro <montenegrodamian@gmail.com>
2014-08-01 20:43:30 -0300

    Adding Image UT and google test library

---
+++ SourceCode/ImageHandler/UnitTest/ImageUnitTests.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+#include "gtest/gtest.h"
+
+#include "Image.h"
+
+
+TEST(ImageTest, checkNeighbors) {
+    PixelArray dummyPixelArray = { 0,1,2,3,4,5,6,7,8 };
+    Image image(dummyPixelArray,3,3);
+
+    PixelArray neighbors = image.getNeighbors(4);
+    ASSERT_EQ(4,neighbors.size());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 1) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 3) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 5) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 7) != neighbors.end());
+
+    neighbors = image.getNeighbors(0);
+    ASSERT_EQ(2,neighbors.size());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 1) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 3) != neighbors.end());
+
+    neighbors = image.getNeighbors(2);
+    ASSERT_EQ(2,neighbors.size());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 1) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 5) != neighbors.end());
+
+    neighbors = image.getNeighbors(6);
+    ASSERT_EQ(2,neighbors.size());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 3) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 7) != neighbors.end());
+
+    neighbors = image.getNeighbors(8);
+    ASSERT_EQ(2,neighbors.size());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 5) != neighbors.end());
+    ASSERT_TRUE(std::find(neighbors.begin(), neighbors.end(), 7) != neighbors.end());
+
+}
